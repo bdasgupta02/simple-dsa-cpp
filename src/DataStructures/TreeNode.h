418df672abd4f735a500c3301c0c0c0dc4d5691d
bdasgupta02 <bdasgupta02@gmail.com>
2022-08-08 13:27:24 +0800

    added tree node

--- src/DataStructures/TreeNode.h
+++ src/DataStructures/TreeNode.h
@@ -3,9 +3,21 @@
 
 namespace SimpleDSA
 {
+  template <typename T>
   struct TreeNode
   {
+    T val;
+    TreeNode<T> *connected;
+
+    TreeNode() {
+      connected = new T[10];
+    }
     
+    TreeNode(T &x)
+    {
+      val = x;
+      connected = new T[10];
+    }
   };
 }
 
